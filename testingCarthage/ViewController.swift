b189eb8b4597eed9608d4aa22e05f58f978fa857
Iziss_Technology <iziss_technology@Iziss-Technologys-Mac-mini.local>
2017-06-20 18:57:29 +0530

    Initial Commit

---
+++ testingCarthage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testingCarthage
+//
+//  Created by Iziss_Technology on 20/06/17.
+//  Copyright © 2017 Pankaj Ramchandani & Co. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

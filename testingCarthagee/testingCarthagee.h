939e980d8df6a9e6b1871f3b5da2bb1a44a2f0e0
Iziss_Technology <iziss_technology@Iziss-Technologys-Mac-mini.local>
2017-06-20 19:01:24 +0530

    test commmit

---
+++ testingCarthagee/testingCarthagee.h
@@ -0,0 +1,19 @@
+//
+//  testingCarthagee.h
+//  testingCarthagee
+//
+//  Created by Iziss_Technology on 20/06/17.
+//  Copyright © 2017 Pankaj Ramchandani & Co. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for testingCarthagee.
+FOUNDATION_EXPORT double testingCarthageeVersionNumber;
+
+//! Project version string for testingCarthagee.
+FOUNDATION_EXPORT const unsigned char testingCarthageeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <testingCarthagee/PublicHeader.h>
+
+
